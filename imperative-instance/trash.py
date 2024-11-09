cc9dc72f03d22cb13acdacce94a11951ce74c43e
benjibyte <benjipixel@proton.me>
2024-11-09 16:05:27 -0600

    removed trash file

--- imperative-instance/trash.py
+++
@@ -1,9 +0,0 @@
-import platform
-get_os = platform.system()
-
-if get_os == "Windows":
-    print("I am using a Windows PC!")
-elif get_os == "macOS":
-    print("I was rich!")
-else:
-    print("I am on linux")
